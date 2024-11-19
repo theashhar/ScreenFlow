7dd0551ac97f3e8b7725770fa4dc96cb92a2e5f5
Ashhar Ali Ahmed <ashhar1111981@gmail.com>
2024-11-20 01:22:08 +0530

    Initialization and boilerplate setup

---
+++ README.md
@@ -0,0 +1,19 @@
+## Starting Development
+
+Start the app in the `dev` environment:
+
+```bash
+npm start
+```
+
+## Packaging for Production
+
+To package apps for the local platform:
+
+```bash
+npm run package
+```
+
+## Maintainers
+
+- [Ashhar Ali Ahmed](https://github.com/theashhar)
---
+++ src/renderer/App.tsx
@@ -0,0 +1,22 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import icon from '../../assets/icon.svg';
+import './App.css';
+
+function Hello() {
+  return (
+    <div>
+      <h1>This is the initialization of the SalesFLow Banger</h1>
+
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
